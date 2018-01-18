fec68f24cb1dcb4ab42cda0b971a9d6a4f68c94b
yewenxiang <yewenxiang23@gmail.com>
2018-01-18 22:06:50 +0800

    Update addNavigationHelpers.js

--- src/addNavigationHelpers.js
+++ src/addNavigationHelpers.js
@@ -14,44 +14,50 @@ import type {
 import NavigationActions from './NavigationActions';
 import invariant from './utils/invariant';
 
-export default function<S: {}>(
-  navigation: NavigationProp<S>
-): NavigationScreenProp<S> {
+export default function<S: *>(navigation: NavigationProp<S, NavigationAction>) {
+  // 添加点击判断
+  let debounce = true;
   return {
-    ...navigation,
-    goBack: (key?: ?string): boolean => {
-      let actualizedKey: ?string = key;
-      if (key === undefined && navigation.state.key) {
-        invariant(
-          typeof navigation.state.key === 'string',
-          'key should be a string'
-        );
-        actualizedKey = navigation.state.key;
-      }
-      return navigation.dispatch(
-        NavigationActions.back({ key: actualizedKey })
-      );
-    },
-    navigate: (
-      routeName: string,
-      params?: NavigationParams,
-      action?: NavigationNavigateAction
-    ): boolean =>
-      navigation.dispatch(
-        NavigationActions.navigate({ routeName, params, action })
-      ),
+      ...navigation,
+      goBack: (key?: ?string): boolean =>
+          navigation.dispatch(
+              NavigationActions.back({
+                  key: key === undefined ? navigation.state.key : key,
+              }),
+          ),
+      navigate: (routeName: string,
+                 params?: NavigationParams,
+                 action?: NavigationAction,): boolean => {
+          if (debounce) {
+              debounce = false;
+              navigation.dispatch(
+                  NavigationActions.navigate({
+                      routeName,
+                      params,
+                      action,
+                  }),
+              );
+              setTimeout(
+                  () => {
+                      debounce = true;
+                  },
+              500,
+              );
+              return true;
+          }
+          return false;
+      },
     /**
      * For updating current route params. For example the nav bar title and
      * buttons are based on the route params.
      * This means `setParams` can be used to update nav bar for example.
      */
-    setParams: (params: NavigationParams): boolean => {
-      invariant(
-        navigation.state.key && typeof navigation.state.key === 'string',
-        'setParams cannot be called by root navigator'
-      );
-      const key = navigation.state.key;
-      return navigation.dispatch(NavigationActions.setParams({ params, key }));
-    },
+    setParams: (params: NavigationParams): boolean =>
+      navigation.dispatch(
+        NavigationActions.setParams({
+          params,
+          key: navigation.state.key,
+        }),
+      ),
   };
 }
